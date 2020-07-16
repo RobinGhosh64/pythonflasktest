a0f3b4f20527c4c4c18123e4f749bfefb56ea224
Robin Ghosh <roghos@microsoft.com>
2020-07-16 13:39:41 -0400

    environ is deprecated

--- application.py
+++ application.py
@@ -1,14 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route("/test/",methods=["GET","POST"])
+@app.route("/",methods=["GET"])
 
 
 
-def application(environ, start_response):
-    headers = [('Content-type', 'text/plain')]
-    status = '200 OK'
-    start_response(status, headers)
-    with app.test_request_context():
-        note = request.args.get('fields')
-        return ["Response is: {0}".format(note).encode('utf-8')]
+def home():
+    return 'Hello'
+
+app.run()
