ec32e7f21052b9b5aec2437e78600d108bfb41ab
Robin Ghosh <roghos@microsoft.com>
2020-07-22 08:58:49 -0400

    Updates

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
-@app.route("/",methods=["GET"])
 
 
+@app.route("/")
+def hello():
+    return "Hello, SmartNinja!"
 
-def home():
-    return 'Hello'
 
-app.run()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
